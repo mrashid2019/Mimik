cc34931d94b11f6616a417eff072571144051312
Mahdia Rashid <mrashid2019@fau.edu>
2023-02-02 23:53:57 -0500

    added pages as a template but not displaying

--- mimik-app/src/App.js
+++ mimik-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+  from 'react-router-dom';
+import Home from './Pages';
+import About from './Pages/about';
+import Login from './Pages/login';
+import Contact from './Pages/contact';
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/login' element={<Login/>} />
+    </Routes>
+    </Router>
+);
 }
-
+  
 export default App;
