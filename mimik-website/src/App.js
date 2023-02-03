de932cacde15a68a84df33d77ad947355d54bab0
Mahdia Rashid <mrashid2019@fau.edu>
2023-02-03 18:57:34 -0500

    Created multi-page layout with basic nav bar

---
+++ mimik-website/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+	from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import AppTour from './pages/apptour';
+import Login from './pages/login';
+import Convert from './pages/convert';
+import Train from './pages/train';
+import SignUp from './pages/signup';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route exact path='/' element={<Home />} />
+		<Route path='/about' element={<About/>} />
+		<Route path='/apptour' element={<AppTour/>} />
+		<Route path='/convert' element={<Convert/>} />
+		<Route path='/train' element={<Train/>} />
+		<Route path='/signup' element={<SignUp/>} />
+		<Route path='/login' element={<Login/>} />
+	</Routes>
+	</Router>
+);
+}
+
+export default App;
---
+++ mimik-website/src/pages/train.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Train = () => {
+return (
+	<div>
+	<h1>Training page for Mimik</h1>
+	</div>
+);
+};
+
+export default Train;
