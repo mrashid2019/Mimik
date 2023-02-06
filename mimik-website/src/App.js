db731cbf08d42e68d07645d68f1d9b258072d281
Mahdia Rashid <mrashid2019@fau.edu>
2023-02-06 16:07:04 -0500

    made changed on login.js

--- mimik-website/src/App.js
+++ mimik-website/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
