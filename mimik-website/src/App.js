f0b24072eaedc7b8aad8d18eb77b80677691c294
Mahdia Rashid <mrashid2019@fau.edu>
2023-02-11 18:16:12 -0500

    adjusted app.js and index.js to make login display

--- mimik-website/src/App.js
+++ mimik-website/src/App.js
@@ -3,29 +3,32 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
-	from 'react-router-dom';
+    from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 import AppTour from './pages/apptour';
-import Login from './pages/login';
 import Convert from './pages/convert';
 import Train from './pages/train';
 import SignUp from './pages/signup';
+import Login from './pages/login';
+import { UserAuthContextProvider } from './context/userAuthContext';
 
 function App() {
 return (
-	<Router>
-	<Navbar />
-	<Routes>
-		<Route exact path='/' element={<Home />} />
-		<Route path='/about' element={<About/>} />
-		<Route path='/apptour' element={<AppTour/>} />
-		<Route path='/convert' element={<Convert/>} />
-		<Route path='/train' element={<Train/>} />
-		<Route path='/signup' element={<SignUp/>} />
-		<Route path='/login' element={<Login/>} />
-	</Routes>
-	</Router>
+    <UserAuthContextProvider>
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/apptour' element={<AppTour/>} />
+        <Route path='/convert' element={<Convert/>} />
+        <Route path='/train' element={<Train/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<Login/>} />
+    </Routes>
+    </Router>
+    </UserAuthContextProvider>
 );
 }
 
