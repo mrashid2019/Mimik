bddf916db79f1e32bfd8e320f61ce33bad49e51d
wdure2018 <wdure2018@fau.edu>
2023-02-18 18:02:58 -0500

    about page, train.py, and svgs

--- Mimik_Voice_Cloner/ASR/train.py
+++ Mimik_Voice_Cloner/ASR/train.py
@@ -1,7 +1,17 @@
 from model import Transcriber
 from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 from pytorch_lightning import Trainer
+from torchaudio.datasets import LIBRISPEECH
+from torch.utils.data import DataLoader
 from argparse import ArgumentParser
+
+def get_trainer_details():
+    help(Trainer)
+
+def get_librispeech_data():
+    return LIBRISPEECH(root='./ASR/datasets/LibriSpeech_tc100',download='True')
+
+
 def train(*args):
     h_params = Transcriber.h_params
     # h_params.update(args.h_params_override)
@@ -15,7 +25,15 @@ def train(*args):
         num_nodes=1, gradient_clip_val=1.0,
         val_check_interval=1
    )
-    trainer.fit(model)
+    
+    data = get_librispeech_data()
+    data_loader = DataLoader(
+    ls_data,
+    batch_size=1,
+    shuffle=True,)
+
+    trainer.fit(model, data_loader)
 
 
-train()
+ls_data = get_librispeech_data()
+print({ls_data})
--- mimik-website/src/pages/about.js
+++ mimik-website/src/pages/about.js
@@ -1,11 +1,39 @@
 import React from "react";
+import './About/about.css'
+import Icon from "../assets/svgs/undraw_collaborators_re_hont"
+import Footer from "../components/Footer"
 
 const About = () => {
+	
 return (
-	<div>
-	<h1>
-		This is the About page 
-	</h1>
+	<div className="About_main">
+		<h1>
+			About Mimik
+		</h1>
+		<div className="aBlock">
+			
+			<h2 style={{display:'flex', alignContent:'center', flexWrap:'wrap',fontSize:'1vmax', margin:'0', lineHeight:'30px', maxWidth:'300px'}}>
+				Mimik is a free to use Voice Cloning web app developed by students at
+				Florida Atlantic University in Boca Raton Florida. 
+				It can be used to create audio
+				deepfakes, or help you create voiceovers for your videos
+			</h2>
+			<Icon width='30%'height='30%'></Icon>
+		</div>
+		<div className="bBlock">
+			<h2 style={{fontSize:'1em', margin:'0', lineHeight:'30px'}}>
+			Mimik combines neural Speech Recognition and 
+			Speech Synthesis technologies in order to create a comprehensive pipeline. 
+			
+			</h2>
+		</div>
+		<div className="cBlock">
+			<h2 style={{fontSize:'1em', margin:'0', lineHeight:'30px'}}>
+				This pipeline lets
+				users clone and borrow the voices of others.
+			</h2>
+		</div>
+		<Footer></Footer>
 	</div>
 );
 };
