cb247d3aa06add733d281811228cd79e167c1d44
durewill <durewill@gmail.com>
2023-02-26 20:54:53 -0500

    about.js changes

--- mimik-website/src/pages/about.js
+++ mimik-website/src/pages/about.js
@@ -1,40 +1,44 @@
 import React from "react";
-import './About/about.css'
-import Icon from "../assets/svgs/undraw_collaborators_re_hont"
-import Footer from "../components/Footer"
+import {ReactComponent as Record} from "./About/undraw_recording_re_5xyq.svg";
+import {ReactComponent as Collaborators} from "./About/undraw_collaborators_re_hont.svg";
+import './About/about.css';
+// import Footer from "../../components/Footer";
+import logo from "./About/AI17A_generated.jpg"
 
 const About = () => {
 	
 return (
-	<div className="About_main">
+	<section className="About_main">
 		<h1>
 			About Mimik
 		</h1>
-		<div className="aBlock">
-			
-			<h2 style={{display:'flex', alignContent:'center', flexWrap:'wrap',fontSize:'1vmax', margin:'0', lineHeight:'30px', maxWidth:'300px'}}>
+		<div className="block aBlock">
+			<h2 className="descriptors">
 				Mimik is a free to use Voice Cloning web app developed by students at
 				Florida Atlantic University in Boca Raton Florida. 
 				It can be used to create audio
 				deepfakes, or help you create voiceovers for your videos
 			</h2>
-			<Icon width='30%'height='30%'></Icon>
+			<Collaborators className="Collaborators"></Collaborators>
 		</div>
-		<div className="bBlock">
-			<h2 style={{fontSize:'1em', margin:'0', lineHeight:'30px'}}>
+		<div className="block bBlock">
+			<img src={logo} alt={"machine learning brain"}></img>
+
+			<h2 className="descriptors">
 			Mimik combines neural Speech Recognition and 
-			Speech Synthesis technologies in order to create a comprehensive pipeline. 
-			
+			Speech Synthesis technologies in order to create a comprehensive and low latency pipeline. 
 			</h2>
 		</div>
-		<div className="cBlock">
-			<h2 style={{fontSize:'1em', margin:'0', lineHeight:'30px'}}>
+		<div className="block cBlock">
+			<h2 className="descriptors">
 				This pipeline lets
-				users clone and borrow the voices of others.
+				users clone and borrow the voices of others at the click of a button.
 			</h2>
+			<Record className="Record_svg"/>
+
 		</div>
-		<Footer></Footer>
-	</div>
+		{/* <Footer></Footer> */}
+	</section>
 );
 };
 
--- mimik_backend/src/modules/transcribe.py
+++ mimik_backend/src/modules/transcribe.py
@@ -10,10 +10,12 @@ AUDIO_DIR = '/home/group6/Voice_Cloning/Mimik/mimik_backend/src/audio/output'
 output = client.main()
 print('The output is:', output)
 
+print('The output is:', output)
+
 default_model = TTS.list_models()[9] #glowTTS model set as default
 print('default audio model:',default_model)
 tts = TTS(default_model)
 audio = tts.tts_to_file(output, file_path=AUDIO_DIR+'/output.wav')
 
 audio = np.array(audio)
-write(AUDIO_DIR+'output.wav', 22500,audio)
+write('output.wav', 22500,audio)
