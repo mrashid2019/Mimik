c8ba9f424d7a37d98b4f68702af759b8a9f2d0e1
Mahdia Rashid <mrashid2019@fau.edu>
2023-04-11 00:18:27 -0400

    added styling to two-factor

--- mimik-website/src/components/TwoFactor/2-fa.js
+++ mimik-website/src/components/TwoFactor/2-fa.js
@@ -1,11 +1,12 @@
 import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs";
 import { CgSpinner } from "react-icons/cg";
+import Footer from "../../components/Footer";
 
 import OtpInput from "otp-input-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
-import { auth } from "../../firebase";
+import { auth} from "../../firebase";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { toast, Toaster } from "react-hot-toast";
 
@@ -15,6 +16,7 @@ export default function PhoneAuth() {
   const [loading, setLoading] = useState(false);
   const [showOTP, setShowOTP] = useState(false);
   const [user, setUser] = useState(null);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   function onCaptchaVerify() {
     if (!window.recaptchaVerifier) {
@@ -73,8 +75,15 @@ export default function PhoneAuth() {
   }
 
   return (
-    <section className="flex items-center justify-center h-screen" style={{backgroundColor:'#ffffff'}}>
-      <div>
+    <>
+    <section style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "80vh",
+      backgroundColor: "#ffffff",
+    }}>
+      <div style={{ border: "1px solid #dfdfdf", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", borderRadius: "5%"}}>
         <Toaster toastOptions={{ duration: 4000 }} />
         <div id="recaptcha-container"></div>
         {user ? (
@@ -83,20 +92,20 @@ export default function PhoneAuth() {
           </h2>
         ) : (
           <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
-            <h1 className="text-center leading-normal text-black font-medium text-3xl mb-6">
+            <h1 className="text-center leading-normal text-black font-medium mb-6" style={{fontSize:"40px", padding:"20px"}}>
               Set up two-factor for <br /> Mimik 
             </h1>
             {showOTP ? (
               <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
+                <div style={{ display: "flex", alignItems: "center", marginBottom: "2%" }}>
                   <BsFillShieldLockFill size={30} />
-                </div>
                 <label
                   htmlFor="otp"
-                  className="font-bold text-xl text-black text-center"
+                  className="font-bold text-xl text-black ml-2 text-center"
                 >
                   Enter your OTP
                 </label>
+                </div>
                 <OtpInput
                   value={otp}
                   onChange={setOtp}
@@ -108,7 +117,7 @@ export default function PhoneAuth() {
                 ></OtpInput>
                 <button
                   onClick={onOTPVerify}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-black rounded"
+                  className="flex gap-1 items-center justify-center py-2.5 mt-3 text-white login-btn"
                 >
                   {loading && (
                     <CgSpinner size={20} className="mt-1 animate-spin" />
@@ -118,19 +127,20 @@ export default function PhoneAuth() {
               </>
             ) : (
               <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
-                  <BsTelephoneFill size={30} />
-                </div>
-                <label
+               <div style={{ display: "flex", alignItems: "center", marginBottom: "2%" }}>
+                <BsTelephoneFill size={30} />
+                  <label
                   htmlFor=""
                   className="font-bold text-xl text-black text-center"
-                >
-                  Verify your phone number
-                </label>
+                  style={{ marginLeft: "10px" }}
+                   >
+                    Verify your phone number
+                  </label>
+                </div>
                 <PhoneInput id={"phone-number"} country={"us"} value={ph} onChange={setPh} />
-                <button
+                <button 
                   onClick={onSignup} style={{marginTop:'2%'}}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-black rounded"
+                  className="flex gap-1 items-center justify-center py-2.5 mt-3 text-white login-btn"
                 >
                   {loading && (
                     <CgSpinner size={20} className="mt-1 animate-spin" />
@@ -143,5 +153,7 @@ export default function PhoneAuth() {
         )}
       </div>
     </section>
+    <Footer/>
+    </>
   );
 };
